f0e43af580c6bcc601785383a418211e730ba1e7
NoteEdu <eduardo.tsilva@hotmail.com>
2021-04-17 21:43:08 -0300

    29. Componente create (Parte 1)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { FinalizadosComponent } from './components/finalizados/finalizados.component';
-import { ReadAllComponent } from './components/read-all/read-all.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CreateComponent } from "./components/create/create.component";
+import { FinalizadosComponent } from "./components/finalizados/finalizados.component";
+import { ReadAllComponent } from "./components/read-all/read-all.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: ReadAllComponent
+    path: "",
+    component: ReadAllComponent,
   },
   {
-    path: 'finalizados',
-    component: FinalizadosComponent
-  }
+    path: "finalizados",
+    component: FinalizadosComponent,
+  },
+  {
+    path: "create",
+    component: CreateComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
@@ -14,7 +14,11 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { FinalizadosComponent } from './components/finalizados/finalizados.component';
+import { FinalizadosComponent } from "./components/finalizados/finalizados.component";
+import { CreateComponent } from "./components/create/create.component";
+import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -23,10 +27,12 @@ import { FinalizadosComponent } from './components/finalizados/finalizados.compo
     FooterComponent,
     ReadAllComponent,
     FinalizadosComponent,
+    CreateComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
@@ -35,6 +41,9 @@ import { FinalizadosComponent } from './components/finalizados/finalizados.compo
     HttpClientModule,
     MatBadgeModule,
     MatSnackBarModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
